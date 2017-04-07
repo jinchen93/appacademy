3842c45da251f3bac3073947b5ce81b3e940f6e0
jin <jinchen93@gmail.com>
2017-04-07 08:49:27 -0700

    Add stackqueue

--- w2/w2d4/project/stackqueue.rb
+++ w2/w2d4/project/stackqueue.rb
@@ -1,6 +1,27 @@
 require_relative 'mystack'
-require_relative 'myqueue'
 
 class StackQueue
+  def initialize
+    @stackin = MyStack.new
+    @stackout = MyStack.new
+  end
 
+  def enqueue(obj)
+    @stackin.push(obj)
+  end
+
+  def dequeue
+    if @stackout.empty?
+      @stackout.push(@stackin.pop) until @stackin.empty?
+    end
+    @stackout.pop
+  end
+
+  def size
+    @stackin.size + @stackout.size
+  end
+
+  def empty?
+    @stackin.empty? && @stackout.empty?
+  end
 end
