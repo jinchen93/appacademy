1479246efec14afe698711e4accd04f5487364ce
Jin Chen <jinchen93@gmail.com>
2017-04-10 16:26:07 -0700

    Finish 07 of SQLZoo

--- w3/w3d1/hw/skeleton/lib/07_joins_exercises.rb
+++ w3/w3d1/hw/skeleton/lib/07_joins_exercises.rb
@@ -40,6 +40,16 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+    SELECT
+      title
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON actors.id = castings.actor_id
+    WHERE
+      actors.name = 'Harrison Ford'
   SQL
 end
 
@@ -48,12 +58,32 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+    SELECT
+      title
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON actors.id = castings.actor_id
+    WHERE
+      name = 'Harrison Ford' AND ord != 1
   SQL
 end
 
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
+    SELECT
+      title, name
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON actors.id = castings.actor_id
+    WHERE
+      yr = 1962 AND ord = 1
   SQL
 end
 
@@ -61,6 +91,20 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
+    SELECT
+      yr, COUNT(yr)
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON actors.id = castings.actor_id
+    WHERE
+      name = 'John Travolta'
+    GROUP BY
+      yr
+    HAVING
+      COUNT(yr) > 1
   SQL
 end
 
@@ -68,6 +112,31 @@ def andrews_films_and_leads
   # List the film title and the leading actor for all of the films 'Julie
   # Andrews' played in.
   execute(<<-SQL)
+    SELECT
+      title, name
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON actors.id = castings.actor_id
+    WHERE
+      movie_id IN (
+        SELECT
+          movie_id
+        FROM
+          movies
+        JOIN
+          castings ON movies.id = castings.movie_id
+        JOIN
+          actors ON actors.id = castings.actor_id
+        WHERE
+          name = 'Julie Andrews'
+      )
+      AND
+      ord = 1
+    ORDER BY
+      title
   SQL
 end
 
@@ -75,6 +144,22 @@ def prolific_actors
   # Obtain a list in alphabetical order of actors who've had at least 15
   # starring roles.
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      actors
+    JOIN
+      castings ON actors.id = castings.actor_id
+    JOIN
+      movies ON movies.id = castings.movie_id
+    WHERE
+      ord = 1
+    GROUP BY
+      name
+    HAVING
+      COUNT(ord) >= 15
+    ORDER BY
+      name ASC
   SQL
 end
 
@@ -82,11 +167,50 @@ def films_by_cast_size
   # List the films released in the year 1978 ordered by the number of actors
   # in the cast (descending), then by title (ascending).
   execute(<<-SQL)
+    SELECT
+      title, COUNT(*)
+    FROM
+      movies
+    JOIN
+      castings on castings.movie_id = id
+    JOIN
+      actors on castings.actor_id = actors.id
+    WHERE
+      yr = 1978
+    GROUP BY
+      movies.title
+    ORDER BY
+      count DESC, title ASC
   SQL
 end
 
 def colleagues_of_garfunkel
   # List all the people who have played alongside 'Art Garfunkel'.
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      actors
+    JOIN
+      castings ON actors.id = castings.actor_id
+    JOIN
+      movies ON movies.id = castings.movie_id
+    WHERE
+      name != 'Art Garfunkel'
+      AND
+      title
+      IN
+      ( SELECT
+          movies.title
+        FROM
+          actors
+        JOIN
+          castings ON actors.id = castings.actor_id
+        JOIN
+          movies ON movies.id = castings.movie_id
+        WHERE
+          name = 'Art Garfunkel')
+    ORDER BY
+      name
   SQL
 end
