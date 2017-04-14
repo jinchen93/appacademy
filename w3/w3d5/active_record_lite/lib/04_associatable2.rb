462062e3de455d17693d3dd626caf27d1883890f
Jin Chen <jinchen93@gmail.com>
2017-04-14 16:38:40 -0700

    Annotate Associatable#has_one_through

--- w3/w3d5/active_record_lite/lib/04_associatable2.rb
+++ w3/w3d5/active_record_lite/lib/04_associatable2.rb
@@ -9,10 +9,15 @@ module Associatable
 #   => Cat belongs to Human
     define_method(name) do
       through_obj = self.send(through_name)
+      # => Returns Human obj
       source_options = through_options.model_class.assoc_options[source_name]
+      # => Returns the Human belongs to House association
       source_foreign_key_name = source_options.foreign_key
+      # => Gets foreign key name so that we can find ID ('house_id')
       source_id = through_obj.send(source_foreign_key_name)
+      # => Gets the house_id #
       source_options.model_class.find(source_id)
+      # => Finds the House obj
     end
   end
 end
