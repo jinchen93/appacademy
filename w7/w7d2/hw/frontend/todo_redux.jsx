c1260510f13ecd427a3800cefa4ce3f56764116d
Jin Chen <jinchen93@gmail.com>
2017-05-08 23:10:38 -0700

    Finish w7d2 hw

---
+++ w7/w7d2/hw/frontend/todo_redux.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+const addLoggingToDispatch = store => (
+  next => (
+    action => {
+      console.log(store.getState());
+      console.log(action);
+      store.dispatch(action);
+      console.log(store.getState());
+    }
+  )
+);
+
+const applyMiddleware = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach( middleware => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, dispatch);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  let store = configureStore(preloadedState);
+  const newStore = applyMiddleware(store, addLoggingToDispatch);
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
