a7a562100c65d5ac6587e07f67098ed28aab64b0
Jin <jinchen93@gmail.com>
2017-05-12 19:03:16 -0700

    Add BenchBnB part 1

---
+++ w7/w7d5/projects/bench_bnb/frontend/bench_bnb.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
+
+import * as SessionAction from './actions/session_actions';
+
+document.addEventListener("DOMContentLoaded", () => {
+  let store;
+
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser} };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
+  window.store = store;
+  window.SessionAction = SessionAction;
+
+  const root = document.getElementById("root");
+  ReactDOM.render(
+    <Root store={store} />,
+    root
+  );
+});
---
+++ w7/w7d5/projects/bench_bnb/frontend/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import GreetingContainer from './greeting_container';
+import SessionFormContainer from './session_form_container';
+
+import { AuthRoute } from '../util/route_util';
+
+const App = () => (
+  <div>
+    <h1>BenchBnB</h1>
+    <GreetingContainer />
+
+    <AuthRoute path='/login' component={SessionFormContainer} />
+    <AuthRoute path='/signup' component={SessionFormContainer} />
+  </div>
+);
+
+export default App;
