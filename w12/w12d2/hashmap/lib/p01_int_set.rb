2f01d948afef57e019ee16f735ef2d5d3057b6f8
Jin Chen <jinchen93@gmail.com>
2017-06-13 22:28:14 -0700

    Add ResizingintSet

--- w12/w12d2/hashmap/lib/p01_int_set.rb
+++ w12/w12d2/hashmap/lib/p01_int_set.rb
@@ -35,7 +35,7 @@ class IntSet
   end
 
   def insert(num)
-    self[num].push(num)
+    self[num] << num
   end
 
   def remove(num)
@@ -73,18 +73,31 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if num_buckets == @count
+    @count += 1
+    self[num] << num
   end
 
   def remove(num)
+    @count -= 1
+    self[num].each_with_index do |el, idx|
+      self[num].delete_at(idx) if el == num
+    end
   end
 
   def include?(num)
+    include = false
+      self[num].each do |el|
+        include = true if el == num
+      end
+    include
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -92,5 +105,13 @@ class ResizingIntSet
   end
 
   def resize!
+    new_num_buckets = @store.length * 2
+    new_store = Array.new(new_num_buckets) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |el|
+        new_store[el % new_num_buckets] << el
+      end
+    end
+    @store = new_store
   end
 end
