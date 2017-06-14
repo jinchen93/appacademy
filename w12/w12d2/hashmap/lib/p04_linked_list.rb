b0b91352ab2496694374d80af80393512ac9e376
Jin Chen <jinchen93@gmail.com>
2017-06-14 10:50:49 -0700

    Add linked list

--- w12/w12d2/hashmap/lib/p04_linked_list.rb
+++ w12/w12d2/hashmap/lib/p04_linked_list.rb
@@ -15,11 +15,17 @@ class Node
   def remove
     # optional but useful, connects previous node to next node
     # and removes self from list.
+    @prev && @prev.next = @next
+    @next && @next.prev = @prev
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
   end
 
   def [](i)
@@ -28,30 +34,64 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next.nil? && @tail.prev.nil?
   end
 
   def get(key)
+    each do |node|
+      return node.val if node.key == key
+    end
   end
 
   def include?(key)
+    include = false
+    each do |node|
+      include = true if node.key == key
+    end
+    include
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    if empty?
+      node.prev = @head
+      node.next = @tail
+      @head.next = node
+      @tail.prev = node
+    else
+      node.prev = last
+      node.next = @tail
+      last.next = node
+      @tail.prev = node
+    end
   end
 
   def update(key, val)
+    each do |node|
+      node.val = val if node && node.key == key
+    end
   end
 
   def remove(key)
+    each do |node|
+      node.remove if node && node.key == key
+    end
   end
 
   def each
+    curr_node = @head
+    until curr_node == last
+      curr_node = curr_node.next
+      yield(curr_node)
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
