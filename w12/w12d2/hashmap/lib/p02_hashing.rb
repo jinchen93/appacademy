a4b07df89219106d88449edad08e179802461c89
Jin Chen <jinchen93@gmail.com>
2017-06-13 23:18:17 -0700

    Add hashing functions

--- w12/w12d2/hashmap/lib/p02_hashing.rb
+++ w12/w12d2/hashmap/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    hashed = length.hash
+    each_with_index do |ele, idx|
+      hashed += (ele + idx).hash
+    end
+    hashed
   end
 end
 
 class String
   def hash
+    hashed = length.hash
+    each_char.with_index do |char, idx|
+      ascii_code = char.ord
+      hashed += (ascii_code +  idx).hash
+    end
+    hashed
   end
 end
 
@@ -16,6 +27,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hashed = length.hash
+    each do |k, v|
+      hashed += (k.hash + v.hash)
+    end
+    hashed
   end
 end
