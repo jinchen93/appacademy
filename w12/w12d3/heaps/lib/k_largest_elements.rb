628e54f72eb544a9e562fbd3867f08bd596a4621
Jin Chen <jinchen93@gmail.com>
2017-06-15 11:17:28 -0700

    Add k largest elements

--- w12/w12d3/heaps/lib/k_largest_elements.rb
+++ w12/w12d3/heaps/lib/k_largest_elements.rb
@@ -7,5 +7,20 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
+  result = []
 
+  prc = Proc.new do |el1, el2|
+    -1 * (el1 <=> el2)
+  end
+
+  heap = BinaryMinHeap.new(&prc)
+  array.each do |ele|
+    heap.push(ele)
+  end
+
+  k.times do
+    result << heap.extract
+  end
+
+  result
 end
