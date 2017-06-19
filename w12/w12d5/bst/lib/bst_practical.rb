4502a85843d8091fa5dd54fcf0414a650490d0e0
Jin Chen <jinchen93@gmail.com>
2017-06-19 11:45:43 -0700

    Finish BST

--- w12/w12d5/bst/lib/bst_practical.rb
+++ w12/w12d5/bst/lib/bst_practical.rb
@@ -1,3 +1,18 @@
+require_relative 'binary_search_tree'
 
 def kth_largest(tree_node, k)
+  count = 0
+
+
+  until count == k
+    tree_node = tree_node.right until tree_node.right.nil?
+    tree_node = tree_node.parent
+
+    count += 1 if tree_node.right
+    return tree_node.right if count == k
+    count += 1
+    return tree_node if count == k
+    tree_node = tree_node.left
+  end
+  nodes[k - 1]
 end
