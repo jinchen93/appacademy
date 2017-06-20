bdfdae83a1cf39ed240951dec40bf60f5cdb746e
Jin Chen <jinchen93@gmail.com>
2017-06-20 11:21:30 -0700

    Add topo sort

--- w13/w13d1/graphs/lib/topological_sort.rb
+++ w13/w13d1/graphs/lib/topological_sort.rb
@@ -3,5 +3,22 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  sorted = []
+  sort_queue = []
 
+  vertices.each do |vertex|
+    sort_queue << vertex if vertex.in_edges.empty?
+  end
+
+  until sort_queue.empty?
+    current_vertex = sort_queue.shift
+    sorted << current_vertex
+    current_vertex.out_edges.each do |edge|
+      to_vertex = edge.to_vertex
+      edge.destroy!
+      sort_queue << to_vertex if to_vertex.in_edges.empty?
+    end
+  end
+
+  sorted
 end
