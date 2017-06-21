3a048efb6c9264cb87aa6c938ca9109266b9eb53
Jin Chen <jinchen93@gmail.com>
2017-06-21 10:47:12 -0700

    Add blair_nums

--- w13/w13d2/dynamic-programming/lib/dynamic_programming.rb
+++ w13/w13d2/dynamic-programming/lib/dynamic_programming.rb
@@ -1,15 +1,32 @@
 class DynamicProgramming
   def initialize
-
+    @blair_cache = { 1 => 1, 2 => 2 }
   end
 
   def blair_nums(n)
+    return @blair_cache[n] if @blair_cache[n]
+    count = 1
+    num = 1
+    until count == n
+      num += 2
+      count += 1
+    end
+    this_blair_num = blair_nums(n - 1) + blair_nums(n - 2) + num
+    @blair_cache[n] = this_blair_num
+    this_blair_num
   end
 
   def frog_hops(n)
+
   end
 
   def frog_cache_builder(n)
+    cache = {
+      1 => [[1]],
+      2 => [[1, 1], [2]],
+      3 => [[1, 1, 1], [1, 2], [3]]
+    }
+
   end
 
   def frog_hops_top_down(n)
@@ -24,3 +41,6 @@ class DynamicProgramming
   def maze_solver(maze, start_pos, end_pos)
   end
 end
+
+x = DynamicProgramming.new
+p x.blair_nums(6)
